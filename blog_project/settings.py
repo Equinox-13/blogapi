132a0dea8a7f4e3de1f751d1aebcb986ee366ab3
Equinox-13 <quaid.johar33@gmail.com>
2020-10-02 22:15:13 +0530

    Added token authentication configuration

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3rd-party apps
     'rest_framework',
+    'rest_framework.authtoken',
 
     # Local
     'posts.apps.PostsConfig',
@@ -131,7 +132,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication']
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 
