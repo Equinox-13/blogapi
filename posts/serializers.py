5602528100ad7542c639e70e1fe89399670e0c80
Equinox-13 <quaid.johar33@gmail.com>
2020-10-24 13:37:14 +0530

    Added Users list view and detail view endpoints

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
@@ -6,3 +7,9 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
+
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
@@ -13,3 +15,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
