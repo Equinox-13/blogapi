c6529d1966cac1908950b4e78973fbbb39beb791
Equinox-13 <quaid.johar33@gmail.com>
2020-10-25 15:46:01 +0530

    Added api title and description, Fixed docs url with an additional config line in settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -155,3 +155,5 @@ authenticated users have write, edit, or delete privileges
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID = 1
+
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
